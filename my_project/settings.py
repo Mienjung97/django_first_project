d15fcbbf5b793bc9eacf934c87c668995b2edced
Sebastian Kefer <sebastian.kefer@gmail.com>
2024-06-13 14:33:35 +0000

    Made project ready for heroku deployment

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k-zw18psql30n*^$phao9fdpws_+ld-5u!3vr04dep8%f%rck('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-mienjung97-djangofirstp-e152s1thrdj.ws-eu114.gitpod.io']
+ALLOWED_HOSTS = ['8000-mienjung97-djangofirstp-e152s1thrdj.ws-eu114.gitpod.io'
+                '.herokuapp.com']
 
 
 # Application definition
